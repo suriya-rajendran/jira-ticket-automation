4d057523f70a8627f85243685aa2af0c2b0c998a
suriya-rajendran <rjayasuriyanarayanan@gmail.com>
2023-12-10 20:20:48 +0530

    Initial commit

---
+++ src/main/java/com/automation/jira/ticket/JiraTicketApplication.java
@@ -0,0 +1,13 @@
+package com.automation.jira.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automation/jira/ticket/JiraTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.jira.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
